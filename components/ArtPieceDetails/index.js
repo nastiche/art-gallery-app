32f701cd4cd46bb615c706626ab46a6f107b812a
Anastasiia Chetverykova <ana.chetverykova@gmail.com>
2023-06-02 23:35:16 +0200

    add css

--- components/ArtPieceDetails/index.js
+++ components/ArtPieceDetails/index.js
@@ -12,16 +12,23 @@ export default function ArtPieceDetails({
   return (
     <ul>
       <li>
-        <div style={{ display: "grid", placeItems: "center" }}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "5px",
+          }}
+        >
           <Image
             src={imageSource}
             alt={name}
             width={dimensions.width * 0.1}
             height={dimensions.height * 0.1}
           />
-          <p style={{ margin: "3px" }}>{`"${name}"`}</p>
-          <p style={{ margin: "3px" }}>by {artist}</p>
-          <p style={{ margin: "3px" }}>{`${year} (${genre})`}</p>
+          <p style={{ margin: "0" }}>{`"${name}"`}</p>
+          <p style={{ margin: "0" }}>by {artist}</p>
+          <p style={{ margin: "0" }}>{`${year} (${genre})`}</p>
           <Link href="/art-pieces" style={{ margin: "20px" }}>
             Back
           </Link>
