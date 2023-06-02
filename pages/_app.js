58fa39025725de29184606ed7b82d67d9e6adbae
Nan Cao <caonanla@gmail.com>
2023-06-02 15:56:28 +0200

    add navigation and test

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,20 @@
+import Layout from "@/components/Layout";
 import GlobalStyle from "../styles";
+import useSWR from "swr";
+
+const URL = `https://example-apis.vercel.app/api/art`;
+
+const fetcher = (URL) => fetch(URL).then((response) => response.json());
 
 export default function App({ Component, pageProps }) {
+  const { data, error } = useSWR(URL, fetcher);
+  if (error) return <div>ERROR: failed to load.</div>;
+  if (!data) return <div>...loading</div>;
   return (
     <>
       <GlobalStyle />
-      <Component {...pageProps} />
+      <Component {...pageProps} data={data} error={error} />
+      <Layout />
     </>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,20 @@
-import useSWR from "swr";
 import ArtPieces from "@/components/ArtPieces";
 import Spotlight from "@/components/Spotlight";
 
-const URL = `https://example-apis.vercel.app/api/art`;
+// const URL = `https://example-apis.vercel.app/api/art`;
 
-const fetcher = (URL) => fetch(URL).then((response) => response.json());
+// const fetcher = (URL) => fetch(URL).then((response) => response.json());
 
-export default function HomePage() {
-  const { data, error } = useSWR(URL, fetcher);
+export default function SportlightPage({ data }) {
   function getRandomPiece(array) {
     return array[Math.floor(Math.random() * array.length)];
   }
-
-  if (error) return <div>ERROR: failed to load.</div>;
-  if (!data) return <div>...loading</div>;
 
   const randomPiece = getRandomPiece(data);
 
   return (
     <>
       <Spotlight {...randomPiece} />
-      <ArtPieces pieces={data} />
     </>
   );
 }
